ea16e63ee36d9b5bcd725680a9af62195e85fc14
Takumi Okayasu <takumi_okys@icloud.com>
2024-10-23 20:52:17 +0900

    feat: first commit

---
+++ create_user.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+echo "${0} start!"
+echo "Var = ${USER_NAME}, ${USER_UID}, ${USER_GID}"
+
+# グループの作成 (既に存在する場合はスキップ)
+if ! getent group ${USER_GID} > /dev/null 2>&1; then
+    echo "Creating group with GID ${USER_GID}"
+    groupadd --gid ${USER_GID} ${USER_NAME}
+else
+    echo "Group with GID ${USER_GID} already exists."
+fi
+
+# ユーザーの作成 (既に存在する場合はスキップ)
+if ! id -u ${USER_NAME} > /dev/null 2>&1; then
+    echo "Creating user ${USER_NAME} with UID ${USER_UID} and GID ${USER_GID}"
+    useradd --uid ${USER_UID} --gid ${USER_GID} -m ${USER_NAME}
+else
+    echo "User ${USER_NAME} already exists."
+fi
+
+# sudo権限の付与 (既に存在するか確認して、存在しなければ追加)
+SUDOERS_DIR="/etc/sudoers.d/"
+
+if [ ! -d SUDOERS_DIR ]; then
+    echo "Creating /etc/sudoers.d directory"
+    sudo mkdir -p SUDOERS_DIR
+fi
+
+SUDOERS_FILE=${SUDOERS_DIR}${USER_NAME}
+SUDOERS_ENTRY="${USER_NAME} ALL=(ALL) NOPASSWD:ALL"
+
+if ! grep -Fxq "$SUDOERS_ENTRY" "$SUDOERS_FILE" 2>/dev/null; then
+    echo "Adding sudoers entry for ${USER_NAME}"
+    echo "$SUDOERS_ENTRY" >> "$SUDOERS_FILE"
+else
+    echo "Sudoers entry for ${USER_NAME} already exists and will be updated."
+    echo "$SUDOERS_ENTRY" > "$SUDOERS_FILE"
+fi
+
+chmod 0440 "$SUDOERS_FILE"
+
+echo "${0} finished!"
+
---
+++ terminal_config.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+echo "${0} start!"
+echo "Var = ${USER_NAME}"
+
+DATE=`TZ='Asia/Tokyo' date '+%Y-%m-%d %H:%M:%S %z'`
+echo "  ${0} create $DATE"
+
+# gitの補完スクリプトをダウンロードし、シェルに追加
+curl -o /usr/share/bash-completion/completions/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+curl -o /usr/share/bash-completion/completions/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+
+# ~/.bashrcにPS1とunset PROMPT_COMMANDを追記
+cat <<-EOF > /home/${USER_NAME}/.bashrc
+source /usr/share/bash-completion/completions/git-completion.bash
+source /usr/share/bash-completion/completions/git-prompt.sh
+GIT_PS1_SHOWDIRTYSTATE=true
+GIT_PS1_SHOWUNTRACKEDFILES=true
+GIT_PS1_SHOWUPSTREAM=auto
+unset PROMPT_COMMAND
+export PS1="
+\[\033[01;32m\]\u@\h \
+\[\033[01;33m\] \w \
+\[\033[01;31m\]\$(__git_ps1 '(%%s)') \
+\n\[\033[01;34m\]\\$ \
+\[\033[00m\]"
+EOF
+
+chmod a+x /usr/share/bash-completion/completions/git*
+
+echo "${0} finished!"
+
