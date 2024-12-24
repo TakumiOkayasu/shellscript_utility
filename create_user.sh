7a4fea39464f80ac3165882520deb2807f2ab6fa
Takumi Okayasu <takumi_okys@icloud.com>
2024-12-24 16:31:28 +0900

    fix: make user

--- create_user.sh
+++ create_user.sh
@@ -17,30 +17,31 @@ fi
 if ! id -u ${USER_NAME} > /dev/null 2>&1; then
     echo "Creating user ${USER_NAME} with UID ${USER_UID} and GID ${USER_GID}"
     useradd --uid ${USER_UID} --gid ${USER_GID} -m ${USER_NAME}
+    
+    # sudo権限の付与 (既に存在するか確認して、存在しなければ追加)
+    SUDOERS_DIR="/etc/sudoers.d/"
+
+    if [ ! -d SUDOERS_DIR ]; then
+        echo "Creating /etc/sudoers.d directory"
+        sudo mkdir -p SUDOERS_DIR
+    fi
+
+    SUDOERS_FILE=${SUDOERS_DIR}${USER_NAME}
+    SUDOERS_ENTRY="${USER_NAME} ALL=(ALL) NOPASSWD:ALL"
+
+    if ! grep -Fxq "$SUDOERS_ENTRY" "$SUDOERS_FILE" 2>/dev/null; then
+        echo "Adding sudoers entry for ${USER_NAME}"
+        echo "$SUDOERS_ENTRY" >> "$SUDOERS_FILE"
+    else
+        echo "Sudoers entry for ${USER_NAME} already exists and will be updated."
+        echo "$SUDOERS_ENTRY" > "$SUDOERS_FILE"
+    fi
+
+    chmod 0440 "$SUDOERS_FILE"
+
 else
-    echo "User ${USER_NAME} already exists."
+    echo "User ${USER_NAME} already exists. Skipped."
 fi
-
-# sudo権限の付与 (既に存在するか確認して、存在しなければ追加)
-SUDOERS_DIR="/etc/sudoers.d/"
-
-if [ ! -d SUDOERS_DIR ]; then
-    echo "Creating /etc/sudoers.d directory"
-    sudo mkdir -p SUDOERS_DIR
-fi
-
-SUDOERS_FILE=${SUDOERS_DIR}${USER_NAME}
-SUDOERS_ENTRY="${USER_NAME} ALL=(ALL) NOPASSWD:ALL"
-
-if ! grep -Fxq "$SUDOERS_ENTRY" "$SUDOERS_FILE" 2>/dev/null; then
-    echo "Adding sudoers entry for ${USER_NAME}"
-    echo "$SUDOERS_ENTRY" >> "$SUDOERS_FILE"
-else
-    echo "Sudoers entry for ${USER_NAME} already exists and will be updated."
-    echo "$SUDOERS_ENTRY" > "$SUDOERS_FILE"
-fi
-
-chmod 0440 "$SUDOERS_FILE"
 
 echo "${0} finished!"
 
